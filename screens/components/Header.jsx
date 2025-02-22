348ff52aca39a0f6f2dd7baa977171b2a2ba477c
pozetiv4iks <stepan.ewqazsx@gmail.com>
2025-02-22 17:32:18 +0300

    add: logo preview

--- screens/components/Header.jsx
+++ screens/components/Header.jsx
@@ -1,17 +1,32 @@
-import { Text, View } from 'react-native'
+import { Text, View } from 'react-native';
 import  styled from 'styled-components/native';
+import LogoHeader from './LogoHeader';
 
 const BlockHeader = styled.View`
 background-color: #FFD000;
 width:100%;
 height:60px;
-`
+align-items:center;
+justify-content: space-between;
+`;
+
+const LogoBlock = styled.View`
+margin-left:90px;
+margin-top:10px;
+`;
+
+const LogoTextBlock = styled.Text`
+
+`;
 
 export const Header = () => {
-
+  
     return (
       <BlockHeader>
-        
+        <LogoBlock>
+          <LogoHeader/>
+          <LogoTextBlock>Рядом!</LogoTextBlock>
+        </LogoBlock>
       </BlockHeader>
     )
 }
